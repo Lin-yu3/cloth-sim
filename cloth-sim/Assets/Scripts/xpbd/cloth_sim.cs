ecd15d34ed0f5aa21d5f6c5176e4afa42ffa4fdb
Lin-yu3 <77429303+Lin-yu3@users.noreply.github.com>
2022-03-10 00:59:59 +0800

    set prefab to draw GameObject

--- cloth-sim/Assets/Scripts/xpbd/cloth_sim.cs
+++ cloth-sim/Assets/Scripts/xpbd/cloth_sim.cs
@@ -7,10 +7,9 @@ public class cloth_sim : MonoBehaviour
     int horizontal_resolution=50;//水平
     int vertical_resolution=50;//垂直
     GameObject[] sphere=new GameObject[2626];
-    Particle[] ball=new Particle[2626];
-    List<Vector3> vertices;
-    Vector3[] myVertices=new Vector3[2626];
-    List<int> triangles;
+    List<Particle> ball=new List<Particle>();
+    Particle[] myball=new Particle[2626];
+    List<int> triangles=new List<int>();
     int[] myTriangles=new int[15150];
     int[,] m_triangle_list=new int[5050,3];
     List<Vector2> uvs;
@@ -18,106 +17,14 @@ public class cloth_sim : MonoBehaviour
     List<DistanceConstraint> distconstraints = new List<DistanceConstraint>();
     List<FixedPointConstraint> fixconstraints = new List<FixedPointConstraint>();
     List<EnvironmentalCollisionConstraint> collconstraints = new List<EnvironmentalCollisionConstraint>();
-    
+    public GameObject myCube;
     void Start()
     {
-        vertices = new List<Vector3>();
-        triangles=new List<int>();
-        for(int i=0;i<ball.Length;i++)
-        {
-            sphere[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            sphere[i].transform.localScale = new Vector3(0.02f, 0.02f, 0.02f);
-            sphere[i].transform.SetParent(transform);
-        }
         genVertices();
-        genTriangles();
-        DrawSphereSetConstraints();
-    }
-    /*void Update()
-    {
-        float m_delta_physics_time = 1/300f; // 公式:delta_frame_time/substep, substep=5
-        //重力模擬       
-        for (int i = 0; i < ball.Length; i++)
+        Particle[] myball=ball.ToArray();
+        for(int i=0;i<myball.Length;i++)
         {
-            ball[i].v = ball[i].v + m_delta_physics_time * ball[i].w * ball[i].f;
-            ball[i].p = ball[i].x + m_delta_physics_time * ball[i].v;
-        }
-        //更新Mesh
-        //mesh.vertices=myVertices;
-        // Reset Lagrange multipliers (only necessary for XPBD)
-        foreach(DistanceConstraint constraint in distconstraints){
-            constraint.m_lagrange_multiplier=0;
-        }
-        foreach(FixedPointConstraint constraint in fixconstraints){
-            constraint.m_lagrange_multiplier=0;
-        }
-        //generateCollisionConstraints();
-        // Project Particles
-        int solverIterators=10;
-        for (int i = 0; i < solverIterators; i++){    
-            foreach(DistanceConstraint constraint in distconstraints){
-                constraint.projectParticles();
-            }
-            foreach(FixedPointConstraint constraint in fixconstraints){
-                constraint.projectParticles();
-            }
-        }
-        //更新 GameObject localPosition & Particles
-        for(int i=0;i<ball.Length;i++)
-        {
-            //更新 GameObject's localPosition
-            sphere[i].transform.localPosition= ball[i].p;
-            //更新 particle
-            ball[i].v = (ball[i].p-ball[i].x) * (1.0f/m_delta_physics_time);
-            ball[i].x = ball[i].p;
-            //Update velocities
-            ball[i].v*=0.9999f;
-        }
-        //Clear EnvironmentalCollisionConstraints
-        //collconstraints.Clear();
-    }*/
-    void DrawSphereSetConstraints()
-    {
-        //設置頂點
-        //for(int i=0;i<vertices.Count;i++){ vertices[i]+=new Vector3(0,2,1);}
-        Vector3[] myVertices=vertices.ToArray();
-        for(int i=0;i<ball.Length;i++)
-        {
-            ball[i] = new Particle(myVertices[i]);
-            print("myVertices["+i+"]: "+ myVertices[i]);
-            sphere[i].transform.localPosition = ball[i].x;     
-        }
-        
-        //設置三角形頂點順序，順時針設置
-        int[] myTriangles=triangles.ToArray();
-        print("myTriangles.Length: "+ myTriangles.Length);
-
-        //釘住右上角,左上角
-        float range_radius = 0.1f;
-        for (int i=0;i<ball.Length;i++)
-        {
-            if ((ball[i].x - new Vector3(1,2,0)).magnitude < range_radius)
-            {
-                fixconstraints.Add( new FixedPointConstraint(ball[i],ball[i].x));
-                ball[i].m = 99999;//質量很大,就不會被DistanceConstraint影響
-                ball[i].w = 1/ball[i].m;
-            }
-            if ((ball[i].x - new Vector3(-1,2,0)).magnitude < range_radius)
-            {
-                fixconstraints.Add( new FixedPointConstraint(ball[i],ball[i].x));
-                ball[i].m = 99999;//質量很大,就不會被DistanceConstraint影響
-                ball[i].w = 1/ball[i].m;
-            }
-        }
-        for ( int i = 0; i < myTriangles.Length / 3; ++i)
-        {
-            Particle p_0 = ball[myTriangles[i * 3 + 0]];
-            Particle p_1 = ball[myTriangles[i * 3 + 1]];
-            Particle p_2 = ball[myTriangles[i * 3 + 2]];
-
-            distconstraints.Add( new DistanceConstraint(p_0, p_1, (p_0.x - p_1.x).magnitude));
-            distconstraints.Add( new DistanceConstraint(p_0, p_2, (p_0.x - p_2.x).magnitude));
-            distconstraints.Add( new DistanceConstraint(p_1, p_2, (p_1.x - p_2.x).magnitude));
+            Instantiate(myCube, ball[i].x, new Quaternion(0,90,0,0));
         }
     }
     void genVertices()
@@ -132,14 +39,16 @@ public class cloth_sim : MonoBehaviour
                 float v = (v_index) / (vertical_resolution);
                 float x = (u - 0.5f) * 2;
                 float y = (v - 0.5f) * 2;
-                vertices.Add(new Vector3(x, 0, y));
+                //vertices.Add(new Vector3(x, 0, y));
+                ball.Add(new Particle( new Vector3(x, 0, y)));
                 //uvs.Add(new Vector2(u, v));
                 // Additional vetex at the even-indexed row
                 if (v_index % 2 == 1 && h_index == horizontal_resolution)
                 {
-                    vertices.Add(new Vector3(0.5f * 2, 0, y));
+                    //vertices.Add(new Vector3(0.5f * 2, 0, y));
+                    ball.Add(new Particle(new Vector3(0.5f * 2, 0, y)));
                     //uvs.Add(new Vector2(1, v));
-                }
+                }  
             }
         }
     }
@@ -198,6 +107,6 @@ public class cloth_sim : MonoBehaviour
                     }
                 }
             }
-        }
+        }    
     }
 }
