486ec3f7bd57fb4f26f3ab902ec2b5903836ca65
Lin-yu3 <77429303+Lin-yu3@users.noreply.github.com>
2022-07-13 00:03:54 +0800

    Change the value of tolerance, and the position of moving sphere will start from (0, 1, 1.8)

--- cloth-sim/Assets/Scripts/xpbd/cloth_hit_sphere.cs
+++ cloth-sim/Assets/Scripts/xpbd/cloth_hit_sphere.cs
@@ -38,7 +38,7 @@ public class cloth_hit_sphere : MonoBehaviour
             sphere=Instantiate(myPrefab, new Vector3(0,1f,0), Quaternion.identity);
         }
         else{
-            sphere=Instantiate(myPrefab, new Vector3(0,1f,0), Quaternion.identity);
+            sphere=Instantiate(myPrefab, new Vector3(0,1f,1.8f), Quaternion.identity);
         }
     }
     void Update()
@@ -299,7 +299,7 @@ public class cloth_hit_sphere : MonoBehaviour
         if(MOVING_SPHERE_COLLISION==false)
         {
             Vector3 center= sphere.transform.localPosition;
-            float tolerance=0.05f;
+            float tolerance=0.01f;
             float radius=0.5f+0.02f;//大圓半徑+小圓半徑?
             for(int i=0; i<ball.Length; i++)
             {
@@ -307,7 +307,7 @@ public class cloth_hit_sphere : MonoBehaviour
                 if (direction.magnitude< radius + tolerance)
                 {
                     Vector3 normal = direction.normalized;
-                    float distance = (center.x*normal.x+center.y*normal.y+center.z*normal.z) + radius+ tolerance;
+                    float distance = (center.x*normal.x+center.y*normal.y+center.z*normal.z) + radius;
                     collconstraints.Add( new EnvironmentalCollisionConstraint(ball[i], normal, distance));
                 }
             }
@@ -319,7 +319,7 @@ public class cloth_hit_sphere : MonoBehaviour
             Vector3 center= sphere.transform.localPosition;
             print("moving sphere center: "+center);
             
-            float tolerance= 0.05f;
+            float tolerance= 0.025f;
             float radius=0.5f+0.02f;//大圓半徑+小圓半徑?
             for(int i=0; i<ball.Length; i++)
             {
@@ -332,7 +332,7 @@ public class cloth_hit_sphere : MonoBehaviour
                 }
             }
             if(collconstraints.Count==0){print("sphere is away from cloth!!!");}
-            if(center==new Vector3(0,1f,-30f)){
+            if(center==new Vector3(0,1f,-10f)){
                 Destroy(sphere);
                 print("DESTROY SPHERE");
             }
